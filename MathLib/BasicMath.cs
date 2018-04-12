1aa3a3f92cda63c11095519edc0ae682c32c1529
lecz <leczfalvy.adam@gmail.com>
2018-04-12 21:12:21 +0200

    Add project files.

---
+++ MathLib/BasicMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class BasicMath
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Mul (int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ MathTest/MathTest.cs
@@ -0,0 +1,59 @@
+﻿using MathLib;
+using Xunit;
+
+namespace MathTest
+{
+    public class MathTest
+    {
+
+        public MathTest()
+        {
+
+        }
+
+        [Fact]
+        public void AddTest()
+        {
+            int a = 1;
+            int b = 22;
+
+            int c = BasicMath.Add(a, b);
+
+            Assert.Equal(23, c);
+        }
+
+        [Fact]
+        public void SubTest()
+        {
+            int a = 1;
+            int b = 22;
+
+            int c = BasicMath.Sub(a, b);
+
+            Assert.Equal(-21, c);
+        }
+
+        [Fact]
+        public void DivTest()
+        {
+            int a = 7;
+            int b = 2;
+
+            int c = BasicMath.Div(a, b);
+
+            Assert.Equal(3, c);
+        }
+
+        [Fact]
+        public void MulTest()
+        {
+            int a = 7;
+            int b = 2;
+
+            int c = BasicMath.Mul(a, b);
+
+            Assert.Equal(14, c);
+        }
+    }
+
+}
